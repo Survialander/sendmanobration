fbe391c8712adc631f8bc0e42da12657f3a213dd
Survialander <philipe0611@gmail.com>
2024-09-14 22:11:00 -0300

    faet: add 'in construction' page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,15 @@
 function Home() {
-  return <h1>👀</h1>;
+  return (
+    <div className="w-full h-[100dvh] flex flex-col justify-center items-center">
+      <div className="relative">
+        <h2 className="text-red-500 text-center">WORK IN PROGRESS</h2>
+        <img src="https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExdzVocnJpOTg1NjNjYWprcGU0aGF2aXA4ZGxmMzk3ZmlxMHQ3aml4NSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/hoIvVOJg0o165vCxme/giphy.webp" />
+        <h2 className="text-red-500 text-center font-bold">
+          SEND THE LOGO VITIN !!!
+        </h2>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
