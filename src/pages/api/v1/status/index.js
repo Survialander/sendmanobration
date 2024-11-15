06709b7212844cdee779b66fec08013014589b49
Survialander <philipe0611@gmail.com>
2024-11-15 10:11:43 -0300

    add `database.js` and `.env` variables

--- src/pages/api/v1/status/index.js
+++ src/pages/api/v1/status/index.js
@@ -1,4 +1,8 @@
-export default function status(request, response) {
+import { query } from "../../../../../infra/database";
+
+export default async function status(request, response) {
+  await query("SELECT 1 + 1 as sum");
+
   response.status(200).json({
     chave: "teste api no next",
   });
