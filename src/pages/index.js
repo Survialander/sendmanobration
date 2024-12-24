57042fd06e57301861d9104f50529d80f4cb6646
Survialander <philipe0611@gmail.com>
2024-12-24 17:42:26 -0300

    feat: add loading dog

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,8 +2,22 @@ function Home() {
   return (
     <div className="w-full h-[100dvh] flex flex-col justify-center items-center">
       <div className="relative">
-        <h2 className="text-red-500 text-center">WORK IN PROGRESS</h2>
+        <div className="flex justify-center items-center">
+          <img
+            src="https://media.tenor.com/EGIf4EM7vJ4AAAAM/white-dog-dancing-dancing-dog.gif"
+            width="70px"
+          />
+          <span className="text-red-500 text-center px-2">
+            WORK IN PROGRESS
+          </span>
+          <img
+            src="https://media.tenor.com/EGIf4EM7vJ4AAAAM/white-dog-dancing-dancing-dog.gif"
+            width="70px"
+          />
+        </div>
+
         <img src="https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExdzVocnJpOTg1NjNjYWprcGU0aGF2aXA4ZGxmMzk3ZmlxMHQ3aml4NSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/hoIvVOJg0o165vCxme/giphy.webp" />
+
         <h2 className="text-red-500 text-center font-bold">
           SEND THE LOGO VITIN !!!
         </h2>
