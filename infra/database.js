e3d2e5d9e6eac5cf205707116fb31f8e4a01d8fc
Survialander <philipe0611@gmail.com>
2025-05-07 11:45:53 -0300

    fix: add temporary db log

--- infra/database.js
+++ infra/database.js
@@ -9,6 +9,14 @@ export async function query(queryObject) {
     password: process.env.POSTGRES_PASSWORD,
   });
 
+  console.log("db", {
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+  });
+
   try {
     await client.connect();
     const res = await client.query(queryObject);
