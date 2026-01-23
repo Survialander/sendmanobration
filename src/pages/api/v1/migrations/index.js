b47b80ce6067055ee9129ed2e06e0cbd9de0708f
Survialander <philipe0611@gmail.com>
2026-01-23 15:30:29 -0300

    fix: adjust connection leak bug

--- src/pages/api/v1/migrations/index.js
+++ src/pages/api/v1/migrations/index.js
@@ -3,39 +3,47 @@ import database from "infra/database";
 import { join } from "node:path";
 
 export default async function migrations(request, response) {
-  const dbClient = await database.getNewClient();
-
-  const migrationsConfig = {
-    dbClient: dbClient,
-    dryRun: true,
-    dir: join("infra", "migrations"),
-    direction: "up",
-    verbose: true,
-    migrationsTable: "pgmigrations",
-  };
-
+  const alowedMethods = ["GET", "POST"];
   const method = request.method;
 
-  if (method === "GET") {
-    const pendingMigrations = await migrationRunner({
-      ...migrationsConfig,
-    });
-    await dbClient.end();
-    return response.status(200).json(pendingMigrations);
+  if (!alowedMethods.includes(method)) {
+    return response.status(405).json({ message: "Method Not Allowed" });
   }
 
-  if (method === "POST") {
-    const migratedMigrations = await migrationRunner({
-      ...migrationsConfig,
-      dryRun: false,
-    });
+  let dbClient;
 
+  try {
+    dbClient = await database.getNewClient();
+
+    const migrationsConfig = {
+      dbClient: dbClient,
+      dryRun: true,
+      dir: join("infra", "migrations"),
+      direction: "up",
+      verbose: true,
+      migrationsTable: "pgmigrations",
+    };
+
+    if (method === "GET") {
+      const pendingMigrations = await migrationRunner({
+        ...migrationsConfig,
+      });
+      return response.status(200).json(pendingMigrations);
+    }
+
+    if (method === "POST") {
+      const migratedMigrations = await migrationRunner({
+        ...migrationsConfig,
+        dryRun: false,
+      });
+      return migratedMigrations.length === 0
+        ? response.status(200).json(migratedMigrations)
+        : response.status(201).json(migratedMigrations);
+    }
+  } catch (error) {
+    console.error(error);
+    throw error;
+  } finally {
     await dbClient.end();
-
-    return migratedMigrations.length === 0
-      ? response.status(200).json(migratedMigrations)
-      : response.status(201).json(migratedMigrations);
   }
-
-  return response.status(405).end();
 }
